f69dd687465b29c561374805a623a40d6d68f0eb
n1kh1lmore <nikupcm10@gmail.com>
2023-04-07 11:11:36 +0530

    Second commit

---
+++ dayone/src/dayone/DataTypes.java
@@ -0,0 +1,24 @@
+// Program to demonstrate on various data types
+package dayone;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// integer will give us the values in whole number
+		int a = 10/2;
+		// float and double will gives us the value in decimal point
+		float b = 10/2;
+		// if we use d after any number it gives precision upto 16 decimal places
+		double c = 10/3d;
+		// if we use f after any number it gives precision upto 7 decimal places
+		float d = 10/3f;
+		
+		
+		System.out.println("The value is " + a);
+		System.out.println("The value is " + b);
+		System.out.println("The value is " + c);
+		System.out.println("The value is " + d);
+	}
+
+}
