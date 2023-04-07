a3add8a54ae4786d093bed530d9ecdd0b578cd3d
n1kh1lmore <nikupcm10@gmail.com>
2023-04-07 11:17:13 +0530

    Third commit

---
+++ dayone/src/dayone/CharDemo.java
@@ -0,0 +1,18 @@
+// Program to demonstrate on character
+package dayone;
+
+public class CharDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char a = 'a';
+		System.out.println(a);
+		
+		char b = 70;
+		System.out.println(b);
+		
+		char c = '\u00A3';
+		System.out.println(c);
+	}
+
+}
---
+++ dayone/src/dayone/PrimitiveDataTypes.java
@@ -0,0 +1,23 @@
+// Program to demonstrate on primitive data type
+package dayone;
+
+public class PrimitiveDataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte a = 127;
+		short b = 32767;
+		int c = 25646785;
+		char d ='A';
+		long e = 75754325454L;
+		boolean flag = true;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(flag);
+	}
+
+}
