6629a25fe8d3dc931683ab288c20e104710de965
n1kh1lmore <nikupcm10@gmail.com>
2023-04-07 11:35:02 +0530

    Second commit

---
+++ daytwo/src/daytwo/CompilerInput.java
@@ -0,0 +1,13 @@
+//program to demonstrate on compile-time input
+package daytwo;
+
+public class CompilerInput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//compile-time input
+				float salary = 67000.50f;
+				System.out.println("Salary is " + salary);
+	}
+
+}
