4635d903128c7db893eaf71364001ad9751483f9
n1kh1lmore <nikupcm10@gmail.com>
2023-04-18 19:34:38 +0530

    First commit

---
+++ AssignmentTwo/src/factorydesignpattern/Cellularplan.java
@@ -0,0 +1,11 @@
+package factorydesignpattern;
+
+abstract class Cellularplan {
+	protected double rate;
+	abstract void getRate();
+	public void processBill(int minutes) {
+		System.out.println(minutes*rate);
+		
+	}
+
+}
---
+++ AssignmentTwo/src/factorydesignpattern/XyzNetwork.java
@@ -0,0 +1,9 @@
+package factorydesignpattern;
+
+public class XyzNetwork extends Cellularplan{
+	public void getRate() {
+		@SuppressWarnings("unused")
+		float rate=1.85f;
+	}
+
+}
