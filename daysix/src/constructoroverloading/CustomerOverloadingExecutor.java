5f44c770e879b385a1086570257af1bdfc3c170e
n1kh1lmore <nikupcm10@gmail.com>
2023-04-25 10:46:48 +0530

    First Commit

---
+++ daysix/src/constructoroverloading/CustomerOverloadingExecutor.java
@@ -0,0 +1,11 @@
+package constructoroverloading;
+
+public class CustomerOverloadingExecutor {
+	public static void main(String[] args) {
+		
+		CustomerOverloadingExample c1 = new CustomerOverloadingExample();
+		CustomerOverloadingExample c2 = new CustomerOverloadingExample(12);
+		CustomerOverloadingExample c3 = new CustomerOverloadingExample(12, 13);
+		
+	}
+}
